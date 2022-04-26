7b7ce02650720941762855ae2671bc3b6fbc6546
Eli Craig <elidcraig@gmail.com>
2022-04-26 12:52:40 -0700

    commit tree a23379253ba2ed290bd6039f00fb1f2c7ae8ca01

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = 'BOB';
+    return customerName;
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = 'Pat';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Dave';
+    return leastFavoriteCustomer;
+}
